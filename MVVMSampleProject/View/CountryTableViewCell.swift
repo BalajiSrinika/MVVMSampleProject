79e05dbd358f68794bdff7e24db0f275bd967886
Balaji Pandian <>
2020-06-26 11:19:48 +0530

    Basic changes

--- MVVMSampleProject/View/CountryTableViewCell.swift
+++ MVVMSampleProject/View/CountryTableViewCell.swift
@@ -10,25 +10,24 @@ import UIKit
 
 class CountryTableViewCell: UITableViewCell {
     
-     @IBOutlet weak var countryLbl : UILabel!
-     @IBOutlet weak var currencyLbl : UILabel!
-
+    @IBOutlet weak var countryLbl : UILabel!
+    @IBOutlet weak var captialLbl : UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
     
     func loadWithModel(model : CountryElement){
-        
-        self.countryLbl.text = model.name
-        self.currencyLbl.text = model.capital
+        countryLbl.text = model.name
+        captialLbl.text = model.capital
     }
-
+    
 }
